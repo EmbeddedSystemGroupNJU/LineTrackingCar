af73559ca8b8c5503ae591f9772f2e1d36c80346
charlielei <lei_cheng2008@126.com>
2018-10-19 13:41:50 +0800

    创建框架

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by leich on 2018/10/19.
+//
+
+int main() {
+    /*
+     * 初始化
+     */
+
+    const int CONTROL_PARAMETER_NUMBER = 3;
+    const double PI = 3.14;
+    double controlParameters[CONTROL_PARAMETER_NUMBER] = {0, 0, PI / 2.0};
+
+    while(true) {
+        /*
+         * 图片处理
+         */
+
+        /*
+         * 小车控制
+         */
+    }
+
+}
