d1e69d49b17243c08e57b093c5a7d5579b262388
waxnkw <3104376275@qq.com>
2018-10-21 21:00:34 +0800

    link compile

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo g++ $1 -o test `pkg-config --cflags --libs opencv` -L. -lwiringPi -lGPIO –lpthread
+#sudo g++ $1 -o test `pkg-config --cflags --libs opencv` -L. -lwiringPi -lGPIO –lpthread
+sudo g++ $1 -std=c++11 -o test `pkg-config --cflags --libs opencv` -IcarControl/*.h carControl/class/*.h -L. carControl/*.cpp carControl/class/*.cpp -lwiringPi -lGPIO –lpthread
